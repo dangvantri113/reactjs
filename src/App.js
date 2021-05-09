ea9e3260ac01dd12d7d21beff5419076bb7e7a09
Dang Van Tri <tri-dang@cybozu.vn>
2021-05-09 13:36:35 +0700

    Reactjs Color And Size Settings Practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ColorPicker from './components/ColorPicker';
+import SizeSettings from './components/SizeSettings';
+import Reset from './components/Reset';
+import Result from './components/Result';
 
 function App() {
+  const [color, setColor] = useState('green');
+  const [size, setSize] = useState(12);
+  const onReset = () => {
+    setColor('green');
+    setSize(12);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="container mt-50">
+          <div className="row">
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+               <ColorPicker color={color} onReceiveColor={setColor}/>
+            </div>
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+               <SizeSettings size={size} onReceiveSize={setSize}/>
+               <Reset onReset={onReset}/>
+            </div>
+            <Result color={color} size={size}/>
+          </div>
+          </div>    
+      </div> 
     </div>
   );
 }
